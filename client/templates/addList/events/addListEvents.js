0d7befb032f1f6282f71fb6507dcc3ab7a6ec0a5
chandrangreat <chandrangreatfree@gmail.com>
2016-02-28 14:24:35 +0530

    LIst addition complete

--- client/templates/addList/events/addListEvents.js
+++ client/templates/addList/events/addListEvents.js
@@ -1,6 +1,8 @@
 Template.add_list_button.events({
   'click .js-show-list-form' : function(event){
+    var boardid = $(event.currentTarget).data('boardid');
     $("#add_list_form").modal('show');
+      $(".modal-body #form-list-name").attr('data-boardid',boardid);
   }
 });
 
@@ -9,9 +11,11 @@ Template.add_list_form.events({
     event.preventDefault();
     var list_name;
     list_name = event.target.list_name.value;
+    var boardid =  $(event.target.list_name).data('boardid');
     $(this).find("input[type=text]").val("");
   List.insert({
-      list_name: list_name
+      list_name: list_name,
+      boardid: boardid
     });
     $("#add_list_form").modal('hide');
     return false;
