f80270e56e0214f13adf012f0112df66a72eee60
chandrangreat <chandrangreatfree@gmail.com>
2016-02-28 17:13:44 +0530

    Issue solved with  adding list to two boards separately

--- client/templates/addList/events/addListEvents.js
+++ client/templates/addList/events/addListEvents.js
@@ -1,8 +1,9 @@
 Template.add_list_button.events({
   'click .js-show-list-form' : function(event){
     var boardid = $(event.currentTarget).data('boardid');
+    $(".modal-body #form-list-name").attr('data-boardid',boardid);
     $("#add_list_form").modal('show');
-      $(".modal-body #form-list-name").attr('data-boardid',boardid);
+
   }
 });
 
@@ -10,13 +11,17 @@ Template.add_list_form.events({
   'submit .js-add-list' : function(event){
     event.preventDefault();
     var list_name;
+    var boardId="";
+        var boardId = event.target.list_name.getAttribute('data-boardid'); //$(event.currentTarget.list_name).data('boardid');
     list_name = event.target.list_name.value;
-    var boardid =  $(event.target.list_name).data('boardid');
+
+    console.log(boardId);
     $(this).find("input[type=text]").val("");
   List.insert({
       list_name: list_name,
-      boardid: boardid
+      boardid: boardId
     });
+    event.target.list_name.value='';
     $("#add_list_form").modal('hide');
     return false;
   }
--- client/templates/list/helper/listHelper.js
+++ client/templates/list/helper/listHelper.js
@@ -1,5 +1,16 @@
 Template.list.helpers({
-   list : function(){
-     return List.find({});
+   list : function(boardId){
+     //var templateInstance= this;
+     //var boardId = $(".list_container").attr("id");
+    // console.log(boardId);
+      return List.find({boardid:boardId});
+    // return List.find({});
    }
   });
+// Template.list.onRendered(function () {
+//
+//   var templateInstance= this;
+//   var boardId = templateInstance.$(".list_container").attr("id");
+//   console.log(boardId);
+//
+// });
